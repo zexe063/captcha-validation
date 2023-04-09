fe60fe2f4ed4e50da8a8ae1ff12e8cf9f619c78a
zexe <zexe063@gmail.com>
2023-04-09 16:53:06 +0530

    all are set

--- captcha.js
+++ captcha.js
@@ -2,8 +2,8 @@ let captchabox = document.querySelector(".captcha");
 let captchacheck = document.querySelector(".checker");
 let refresh = document.querySelector(".refresh");
 let check = document.querySelector('.check');
-
-
+let container = document.querySelector('.container');
+console.log(container);
 
 
 function randomcaptcha(){
@@ -18,4 +18,21 @@ function randomcaptcha(){
 
 refresh.addEventListener("click",function(){
    captchabox.value = randomcaptcha();
+})
+
+check.addEventListener("click", function(){
+    let error = document.querySelector(".error");
+
+    if(captchacheck.value.match(captchabox.value)){
+     error.innerHTML = "Very Nice captcha matched"
+     container.classList.add('valid');   
+    }
+
+     else{
+        error.innerHTML = "Ohh! Captcha Are Not Matched"
+        container.classList.remove('valid');
+    }
+
+
+
 })
