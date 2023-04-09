1df341a1088f07a3cc5142fb13ce221bbad292d4
zexe <zexe063@gmail.com>
2023-04-09 16:09:48 +0530

    captcha rae created and value re showing in the caotcha box

--- captcha.js
+++ captcha.js
@@ -2,3 +2,20 @@ let captchabox = document.querySelector(".captcha");
 let captchacheck = document.querySelector(".checker");
 let refresh = document.querySelector(".refresh");
 let check = document.querySelector('.check');
+
+
+
+
+function randomcaptcha(){
+    let value = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqurstuvwxyz1234567890";
+    let newcaptcha = "";
+
+    for(i=0; i<8; i++){
+    newcaptcha = newcaptcha+value[Math.floor(Math.random()*value.length)];
+    }
+    return newcaptcha;
+}
+
+refresh.addEventListener("click",function(){
+   captchabox.value = randomcaptcha();
+})
